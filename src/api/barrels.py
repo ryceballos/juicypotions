dc7ac30960c56343e3f7472f1165876d542a0115
Ryan Ceballos <rayito.marley@gmail.com>
2024-04-08 15:53:25 -0700

    Version 1 V5

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -21,7 +21,16 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    """ """
+    """Gold and ML"""
+    new_green_ml = 0
+    gold_spent = 0
+    for Barrel in barrels_delivered:
+        new_green_ml += (Barrel.quantity * Barrel.ml_per_barrel)
+        gold_spent += (Barrel.quantity * Barrel.price)
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(
+            "UPDATE global_inventory SET gold -= gold_spent, num_green_ml += new_green_ml"))
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
     return "OK"
@@ -32,30 +41,30 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
-
+    green_barrel_purchase = 0
     with db.engine.begin() as connection:
         #Mix availible green ml
-        green_ml_quantity = connection.execute(sqlalchemy.text(
-            "SELECT num_green_ml FROM global_inventory")).scalar()
+        # green_ml_quantity = connection.execute(sqlalchemy.text(
+        #     "SELECT num_green_ml FROM global_inventory")).scalar()
         
-        if green_ml_quantity is not None and green_ml_quantity >= 100:
-            new_potions = green_ml_quantity // 100
+        # if green_ml_quantity is not None and green_ml_quantity >= 100:
+        #     new_potions = green_ml_quantity // 100
             
-            connection.execute(sqlalchemy.text(
-                "UPDATE global_inventory SET num_green_potions = num_green_potions + :potions, num_green_ml - (:potions * 100)"),
-                {"potions": new_potions}
-            )
+        #     connection.execute(sqlalchemy.text(
+        #         "UPDATE global_inventory SET num_green_potions = num_green_potions + :potions, num_green_ml - (:potions * 100)"),
+        #         {"potions": new_potions}
+        #     )
 
         green_potions_num = connection.execute(sqlalchemy.text(
             "SELECT num_green_potions FROM global_inventory")).scalar()
 
         #If the inventory from db table is less than 10, plan to purchase 1
         if green_potions_num is not None and green_potions_num < 10:
-            return [
-                {
-                    "sku": "SMALL_GREEN_BARREL",
-                    "quantity": 1,
-                }
-            ]
-    return "No Purchase"
+            green_barrel_purchase = 1
+    return [
+        {
+            "sku": "SMALL_GREEN_BARREL",
+            "quantity": green_barrel_purchase,
+        }
+    ]
 
