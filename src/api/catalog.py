3529daf37e78f16713f878444330223d52f84efc
Ryan Ceballos <rayito.marley@gmail.com>
2024-04-13 23:36:32 -0700

    VERSION 1 V25

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,22 +9,21 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
-
+    catalog = []
     with db.engine.begin() as connection:
         green_quantity = connection.execute(sqlalchemy.text(
             "SELECT num_green_potions FROM global_inventory")).scalar()
 
         if green_quantity == 0:
-            return []
+            return catalog
         else:
             green_quantity = 1
 
-        return [
-                {
-                    "sku": "Green_POTION_0",
-                    "name": "Green potion",
-                    "quantity": green_quantity,
-                    "price": 50,
-                    "potion_type": [0, 100, 0, 0],
-                }
-            ]
+        catalog.append({
+            "sku": "Green_POTION_0",
+            "name": "Green potion",
+            "quantity": green_quantity,
+            "price": 50,
+            "potion_type": [0, 100, 0, 0],
+        })
+    return catalog
